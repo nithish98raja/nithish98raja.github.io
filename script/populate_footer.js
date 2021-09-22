c2ccdf6f30a23013cbfbe5b98891a62a87a3c44a
nithish <nithish98raja@gmail.com>
2021-09-22 19:34:26 +0530

    add styling

--- script/populate_footer.js
+++ script/populate_footer.js
@@ -13,17 +13,20 @@ function generate_footer(){
     let icon = document.createElement("i");
     icon.setAttribute("id", key+"_icon");
     icon.setAttribute("aria-hidden", "true");
+    icon.classList.add("footer_icon");
     // Add classes for icon
     icon.classList.add("fa");
     icon.classList.add("fa-"+footer_content[key].icon);
     // Create element for text
     let text = document.createElement("p");
     text.setAttribute("id", key+"_text");
+    text.classList.add("footer_text");
     // Add text
     text.textContent = footer_content[key].text;
     // Create div to hold data
     let div = document.createElement("div");
     div.setAttribute("id", key);
+    div.classList.add("footer_element");
     // Add icon and text to div
     div.appendChild(icon);
     div.appendChild(text);
