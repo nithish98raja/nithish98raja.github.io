2fc537a0486915c15e3c4ea9851237f69d54d47e
nithish <nithish98raja@gmail.com>
2021-09-23 15:37:24 +0530

    update home handler to display profile image and description

--- script/generate_content.js
+++ script/generate_content.js
@@ -5,9 +5,24 @@
 
 // Handler to generate content for home section
 function home_handler(){
-  let para = document.createElement("p");
-  para.textContent = "hoooome";
-  return para;
+  // Create element to hold profile image
+  let profile_image = document.createElement("img");
+  profile_image.setAttribute("id", "profile_image");
+  profile_image.setAttribute("alt", main_content["home"]["name"]);
+  profile_image.setAttribute("src", main_content["home"]["image"]);
+  // Create element to hold description
+  let description = document.createElement("p");
+  description.setAttribute("id", "description");
+  // Add text
+  description.textContent = main_content["home"]["description"];
+  // Create container for image and description
+  let container = document.createElement("article");
+  // Add image to container
+  container.appendChild(profile_image);
+  // Add description to container
+  container.appendChild(description);
+  // Return container
+  return container;
 }
 
 // Handler to generate content for education section
